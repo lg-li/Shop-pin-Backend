e342f1ef1054c0f6f817b04300ff2b9c61d4f01c
Cheelem <cheelem@outlook.com>
2019-05-23 10:08:15 +0800

    Project Structure Initialize

---
+++ src/main/java/cn/edu/neu/shop/pin/PinApplication.java
@@ -0,0 +1,13 @@
+package cn.edu.neu.shop.pin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PinApplication.class, args);
+    }
+
+}
