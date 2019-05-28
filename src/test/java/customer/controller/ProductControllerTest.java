d1fb2b70d96e12a2dd7d0e64a218eee70be7002d
RicardoCFeng <594946076@qq.com>
2019-05-28 10:21:43 +0800

    未完成的测试用例

--- src/test/java/customer/controller/ProductControllerTest.java
+++ src/test/java/customer/controller/ProductControllerTest.java
@@ -1,4 +1,57 @@
 package customer.controller;
 
+
+import cn.edu.neu.shop.pin.PinApplication;
+import cn.edu.neu.shop.pin.model.PinProduct;
+import cn.edu.neu.shop.pin.model.PinSettingsProductCategory;
+import org.hamcrest.Matchers;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = PinApplication.class)
 public class ProductControllerTest {
+
+    @Autowired
+    private WebApplicationContext context;
+    private MockMvc mvc;
+
+    @Before
+    public void setUp() throws Exception {
+        mvc = MockMvcBuilders.webAppContextSetup(context).build();
+    }
+
+    @Test
+    public void testGetCategoryByLayer() throws Exception {
+
+        MvcResult result = mvc.perform(MockMvcRequestBuilders.get("/commons/product/category/get-all-by-layer")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andReturn();
+
+        System.out.println(result.getResponse().getContentAsString());
+    }
+
+    @Test
+    public void testGetProductInfoByProductId() throws Exception {
+
+    }
+
+    @Test
+    public void testGetCommentByProductId() throws Exception {
+
+    }
 }
