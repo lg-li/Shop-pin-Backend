67f35aa0734154219ddde064e33e5207accbec95
ydy <978531051@qq.com>
2019-05-28 12:59:04 +0800

    many many new things 骚鸡们

--- src/main/java/cn/edu/neu/shop/pin/mapper/PinUserMapper.java
+++ src/main/java/cn/edu/neu/shop/pin/mapper/PinUserMapper.java
@@ -2,9 +2,11 @@ package cn.edu.neu.shop.pin.mapper;
 
 import cn.edu.neu.shop.pin.model.PinUser;
 import cn.edu.neu.shop.pin.util.base.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Component;
 
 @Component
+@Mapper
 public interface PinUserMapper extends BaseMapper<PinUser> {
     PinUser findByEmail(String email);      //通过Email找用户
     Boolean existsByEmail(String email);    //是否存在有这个Email的用户
