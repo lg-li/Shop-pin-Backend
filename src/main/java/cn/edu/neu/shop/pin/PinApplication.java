d3b9afead1ac7db3a5e176aba94f40a571d58412
RicardoCFeng <594946076@qq.com>
2019-05-28 11:40:19 +0800

    别删我的test

--- src/main/java/cn/edu/neu/shop/pin/PinApplication.java
+++ src/main/java/cn/edu/neu/shop/pin/PinApplication.java
@@ -1,8 +1,8 @@
 package cn.edu.neu.shop.pin;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
 @MapperScan("cn.edu.neu.shop.pin.mapper")
--- src/test/java/customer/controller/ProductControllerTest.java
+++ src/test/java/customer/controller/ProductControllerTest.java
@@ -1,9 +1,18 @@
 package customer.controller;
 
 import cn.edu.neu.shop.pin.mapper.PinUserMapper;
+import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 @Service
 public class ProductControllerTest {
@@ -13,5 +22,44 @@ public class ProductControllerTest {
     @Test
     public void MyTest(){
         System.out.println(pinUserMapper.findByEmail("liyifei_1999@163.com"));
+    }
+
+    @Autowired
+    private WebApplicationContext context;
+    private MockMvc mvc;
+
+    @Before
+    public void setUp() throws Exception {
+        mvc = MockMvcBuilders.webAppContextSetup(context).build();
+    }
+
+
+    @Test
+    public void testGetCategoryByLayer() throws Exception {
+
+        MvcResult result = mvc.perform(MockMvcRequestBuilders.get("/commons/product/category/get-all-by-layer")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andReturn();
+
+        System.out.println(result.getResponse().getContentAsString());
+    }
+
+    @Test
+    public void testGetProductInfoByProductId() throws Exception {
+
+        String result = mvc.perform(MockMvcRequestBuilders.get("/commons/product/1")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andDo(MockMvcResultHandlers.print())
+                .andReturn().getResponse().getContentAsString();
+
+        System.out.println("返回的json=" + result);
+    }
+
+    @Test
+    public void testGetCommentByProductId() throws Exception {
+
     }
 }
