601475920aa4871d0703cb7e4d9a771d3f786406
flyhero99 <yifeilichina@gmail.com>
2019-05-29 16:49:20 +0800

    FUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUCK

--- src/test/java/cn/edu/neu/shop/pin/mapper/PinProductMapperTest.java
+++ src/test/java/cn/edu/neu/shop/pin/mapper/PinProductMapperTest.java
@@ -4,6 +4,8 @@ import cn.edu.neu.shop.pin.PinApplication;
 import cn.edu.neu.shop.pin.customer.service.ProductService;
 import cn.edu.neu.shop.pin.model.PinProduct;
 import java.util.List;
+
+import com.github.pagehelper.PageHelper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
@@ -14,25 +16,30 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringBootTest(classes= PinApplication.class)
-@EnableAutoConfiguration
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@SpringBootTest(classes= PinApplication.class)
+//@EnableAutoConfiguration
+
+@RunWith(SpringRunner.class)
+@MybatisTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class PinProductMapperTest {
 
     @Autowired
     private PinProductMapper pinProductMapper;
 
-    @Autowired
-    private ProductService productService;
+//    @Autowired
+//    private ProductService productService;
 
     @Test
     public void testPage() {
-//        List<PinProduct> list = productService.getHotProductsByPage();
-//        System.out.println("Size: " + list.size());
-//        for(PinProduct p : list) {
-//            System.out.println();
-//            System.out.println(p);
-//            System.out.println();
-//        }
+        PageHelper.startPage(1, 10);
+        List<PinProduct> list = pinProductMapper.getHotProducts();
+        System.out.println("Size: " + list.size());
+        for(PinProduct p : list) {
+            System.out.println();
+            System.out.println(p);
+            System.out.println();
+        }
     }
 }
