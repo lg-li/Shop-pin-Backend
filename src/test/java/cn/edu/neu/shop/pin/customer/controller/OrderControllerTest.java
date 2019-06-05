62562a332b8501cce1049bcbf67b4c019b661200
RicardoCFeng <594946076@qq.com>
2019-06-05 11:16:47 +0800

    测试

--- src/test/java/cn/edu/neu/shop/pin/customer/controller/OrderControllerTest.java
+++ src/test/java/cn/edu/neu/shop/pin/customer/controller/OrderControllerTest.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
@@ -24,20 +25,7 @@ import static org.junit.Assert.*;
 @SpringBootTest(classes = PinApplication.class)
 @WebAppConfiguration
 @EnableAutoConfiguration
-public class OrderControllerTest {
-    private MockMvc mvc;
-    @Autowired
-    private WebApplicationContext context;
-    @Autowired
-    protected UserService userService;
-
-    private String token;
-
-    @Before
-    public void setUp() throws Exception {
-        this.mvc = MockMvcBuilders.webAppContextSetup(this.context).build();
-        token = userService.signIn(1, "liyifei99");
-    }
+public class OrderControllerTest extends UserCredentialNeededTest {
 
     @Test
     public void getGroupOrderInfo() throws Exception {
