46566a64580eb9eca10b364364a5509f28e43696
RicardoCFeng <594946076@qq.com>
2019-06-09 14:46:08 +0800

    管理端 获取近七天评论数

--- src/main/java/cn/edu/neu/shop/pin/mapper/PinUserProductCommentMapper.java
+++ src/main/java/cn/edu/neu/shop/pin/mapper/PinUserProductCommentMapper.java
@@ -4,6 +4,10 @@ import cn.edu.neu.shop.pin.model.PinUserProductComment;
 import cn.edu.neu.shop.pin.util.base.BaseMapper;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 public interface PinUserProductCommentMapper extends BaseMapper<PinUserProductComment> {
+
+    List<PinUserProductComment> getNumberOfComment(Integer storeId);
 }
