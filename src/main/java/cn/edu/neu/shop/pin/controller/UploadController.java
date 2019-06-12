a64427cd79a3dbe57868996010767ca120f45b17
Cheelem <cheelem@outlook.com>
2019-06-12 13:20:04 +0800

    小程序图片上传控制器

---
+++ src/main/java/cn/edu/neu/shop/pin/controller/UploadController.java
@@ -0,0 +1,19 @@
+package cn.edu.neu.shop.pin.controller;
+
+import cn.edu.neu.shop.pin.util.img.ImgUtil;
+import com.alibaba.fastjson.JSONObject;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/commons/upload")
+
+public class UploadController {
+    @PostMapping("/image/base64")
+    public JSONObject uploadStoreInfo(@RequestBody JSONObject uploadingInfo){
+        String base64Img = uploadingInfo.getString("image");
+        return ImgUtil.upload(base64Img,"https://sm.ms/api/upload");
+    }
+}
