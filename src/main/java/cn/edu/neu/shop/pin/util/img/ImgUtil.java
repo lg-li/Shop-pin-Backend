2f0449eea58ad8de7f36f155dbac236f1e668d8f
ydy <978531051@qq.com>
2019-06-13 10:13:30 +0800

    增加product sku definition sku

--- src/main/java/cn/edu/neu/shop/pin/util/img/ImgUtil.java
+++ src/main/java/cn/edu/neu/shop/pin/util/img/ImgUtil.java
@@ -14,7 +14,7 @@ import java.io.*;
 import java.util.Arrays;
 import java.util.Base64;
 
-public class ImgUti {
+public class ImgUtil {
     public static String getSuffix(byte[] source) {
 
         byte[] byteSuffix = Arrays.copyOf(source, 3);
