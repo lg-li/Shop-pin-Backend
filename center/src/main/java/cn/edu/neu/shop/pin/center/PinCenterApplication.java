c7352688d488ba3f32f8d61019fee5902cd2f8b0
Cheelem <cheelem@outlook.com>
2019-06-19 12:57:52 +0800

    项目结构重构，加入负载均衡发现服务模块

---
+++ center/src/main/java/cn/edu/neu/shop/pin/center/PinCenterApplication.java
@@ -0,0 +1,13 @@
+package cn.edu.neu.shop.pin.center;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PinCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PinCenterApplication.class, args);
+    }
+
+}
---
+++ center/src/test/java/cn/edu/neu/shop/pin/center/PinCenterApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.edu.neu.shop.pin.center;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PinCenterApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
