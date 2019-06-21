049e387b41cddac1166fb73803c0cac370d9abfa
Cheelem <cheelem@outlook.com>
2019-06-21 16:58:35 +0800

    禁用消息队列和eureka

--- provider/src/main/java/cn/edu/neu/shop/pin/PinProviderApplication.java
+++ provider/src/main/java/cn/edu/neu/shop/pin/PinProviderApplication.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import tk.mybatis.spring.annotation.MapperScan;
 
-@EnableEurekaClient
+//@EnableEurekaClient
 @SpringBootApplication
 @MapperScan("cn.edu.neu.shop.pin.mapper")
 @EnableScheduling
