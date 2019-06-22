eff04046332f0107ed4e9386c3e3e60c50a41e71
Cheelem <cheelem@outlook.com>
2019-06-22 18:23:02 +0800

    增加consumer端

---
+++ consumer/src/main/java/cn/edu/neu/shop/pin/consumer/controller/ConsumerController.java
@@ -0,0 +1,6 @@
+package cn.edu.neu.shop.pin.consumer.controller;
+
+public class ConsumerController {
+
+    
+}
---
+++ consumer/src/main/java/cn/edu/neu/shop/pin/consumer/service/ProviderService.java
@@ -0,0 +1,4 @@
+package cn.edu.neu.shop.pin.consumer.service;
+
+public class ProviderService {
+}
