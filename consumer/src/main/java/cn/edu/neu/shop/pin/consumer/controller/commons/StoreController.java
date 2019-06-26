1d6fea9c445b5776014eee90daf464df715cfb7d
ydy <978531051@qq.com>
2019-06-26 14:55:20 +0800

    spring cloud

---
+++ consumer/src/main/java/cn/edu/neu/shop/pin/consumer/controller/commons/StoreController.java
@@ -0,0 +1,44 @@
+package cn.edu.neu.shop.pin.consumer.controller.commons;
+
+import cn.edu.neu.shop.pin.consumer.service.commons.ProductControllerService;
+import cn.edu.neu.shop.pin.consumer.service.commons.StoreControllerService;
+import com.alibaba.fastjson.JSONObject;
+import feign.Client;
+import feign.Feign;
+import feign.codec.Decoder;
+import feign.codec.Encoder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.openfeign.FeignClientsConfiguration;
+import org.springframework.cloud.openfeign.support.SpringMvcContract;
+import org.springframework.context.annotation.Import;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Import(FeignClientsConfiguration.class)
+@RestController
+@RequestMapping("/commons/store")
+public class StoreController {
+    private StoreControllerService storeControllerService;
+
+    @Autowired
+    public StoreController(
+            Decoder decoder, Encoder encoder, Client client) {
+        this.storeControllerService = Feign.builder().client(client)
+                .encoder(encoder)
+                .decoder(decoder)
+                .contract(new SpringMvcContract())
+                .target(StoreControllerService.class, "http://Pin-Provider");
+    }
+
+    @GetMapping("/{storeId}/products/{pageNum}/{pageSize}")
+    public JSONObject getProductInfoByStoreId(@PathVariable(value = "storeId") Integer storeId, @PathVariable(value = "pageNum") Integer pageNum, @PathVariable(value = "pageSize") Integer pageSize) {
+        return storeControllerService.getProductInfoByStoreId(storeId, pageNum, pageSize);
+    }
+
+    @GetMapping("/{storeId}")
+    public JSONObject getStoreInfoByStoreId(@PathVariable Integer storeId) {
+        return storeControllerService.getStoreInfoByStoreId(storeId);
+    }
+}
---
+++ consumer/src/main/java/cn/edu/neu/shop/pin/consumer/controller/commons/UserAddressController.java
@@ -0,0 +1,58 @@
+package cn.edu.neu.shop.pin.consumer.controller.commons;
+
+import cn.edu.neu.shop.pin.consumer.service.commons.ProductControllerService;
+import cn.edu.neu.shop.pin.consumer.service.commons.UserAddressControllerService;
+import com.alibaba.fastjson.JSONObject;
+import feign.Client;
+import feign.Feign;
+import feign.codec.Decoder;
+import feign.codec.Encoder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.openfeign.FeignClientsConfiguration;
+import org.springframework.cloud.openfeign.support.SpringMvcContract;
+import org.springframework.context.annotation.Import;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Import(FeignClientsConfiguration.class)
+@RestController
+@RequestMapping("/commons/user")
+public class UserAddressController {
+    private UserAddressControllerService userAddressControllerService;
+
+    @Autowired
+    public UserAddressController(
+            Decoder decoder, Encoder encoder, Client client) {
+        this.userAddressControllerService = Feign.builder().client(client)
+                .encoder(encoder)
+                .decoder(decoder)
+                .contract(new SpringMvcContract())
+                .target(UserAddressControllerService.class, "http://Pin-Provider");
+    }
+
+    @GetMapping("/default-address")
+    public JSONObject getDefaultAddress() {
+        return userAddressControllerService.getDefaultAddress();
+    }
+
+    @GetMapping("/address")
+    public JSONObject getAllAddresses() {
+        return userAddressControllerService.getAllAddresses();
+    }
+
+    @PostMapping("/address")
+    public JSONObject createAddress(@RequestBody JSONObject requestJSON) {
+        return userAddressControllerService.createAddress(requestJSON);
+    }
+
+    @DeleteMapping("/address")
+    public JSONObject deleteAddress(@RequestBody JSONObject requestJSON) {
+        return userAddressControllerService.deleteAddress(requestJSON);
+    }
+
+    @PutMapping("/address")
+    public JSONObject updateAddress(@RequestBody JSONObject requestJSON) {
+        return userAddressControllerService.updateAddress(requestJSON);
+    }
+}
